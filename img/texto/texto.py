e3e7829b039653ccb3b0eb22ebccc7c463f27341
Eduardo31-del <eduardoribeirolopes472@gmail.com>
2025-06-17 14:49:54 -0400

    presente

--- img/texto/texto.py
+++ img/texto/texto.py
@@ -1,79 +1,8 @@
-# Exercício 1
-def calcular_fatorial():
-    n = int(input("Digite um número inteiro positivo: "))
-    fatorial = 1
-    for i in range(1, n + 1):
-        fatorial *= i
-    print(f"O fatorial de {n} é {fatorial}")
+# C  essentials 2
+# https://www.netacad.com/courses/c-essentials-2?courseLang=en-US&instance_id=8a2f2324-acbc-4c6f-aadf-513b80916259
 
-# Exercício 2
-def soma_n_numeros():
-    n = int(input("Digite um número inteiro: "))
-    soma = 0
-    for i in range(1, n + 1):
-        soma += i
-    print(f"A soma dos {n} primeiros números naturais é {soma}")
+# C++ essentials 1
+# https://www.netacad.com/courses/c-plus-plus-essentials-1?courseLang=en-US&instance_id=b3c9f6e5-8a31-4722-9e29-32ae2c310a8a 
 
-# Exercício 3
-def tabuada():
-    n = int(input("Digite um número inteiro: "))
-    print(f"Tabuada do {n}:")
-    for i in range(1, 11):
-        print(f"{n} x {i} = {n * i}")
-        break
-
-# Exercício 4
-def contagem_regressiva():
-    n = int(input("Digite um número inteiro: "))
-    for i in range(n, -1, -1):
-        print(i)
-
-# Exercício 5
-def maior_menor_numero():
-    numeros = []
-    for i in range(5):
-        num = int(input(f"Digite o {i+1}º número: "))
-        numeros.append(num)
-    print(f"Maior número: {max(numeros)}")
-    print(f"Menor número: {min(numeros)}")
-
-# Exercício 6
-def verificar_primo():
-    n = int(input("Digite um número inteiro: "))
-    if n <= 1:
-        print(f"{n} não é primo.")
-        return
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            print(f"{n} não é primo.")
-            return
-    print(f"{n} é primo.")
-
-# Exercício 7
-def fibonacci_ate_oito():
-    fib = [0, 1]
-    while len(fib) < 8:
-        fib.append(fib[-1] + fib[-2])
-    print("Sequência de Fibonacci até o oitavo número:", fib)
-
-# Exercício 8
-def inverter_lista():
-    lista = [int(x) for x in input("Digite os números separados por espaço: ").split()]
-    lista.reverse()
-    print("Lista invertida:", lista)
-
-# Exercício 9
-def verificar_palindromo():
-    palavra = input("Digite uma palavra: ").strip().lower()
-    if palavra == palavra[::-1]:
-        print(f"'{palavra}' é um palíndromo.")
-    else:
-        print(f"'{palavra}' não é um palíndromo.")
-
-# Exercício 10
-def ordenar_lista():
-    lista = [int(x) for x in input("Digite números inteiros separados por espaço: ").split()]
-    crescente = sorted(lista)
-    decrescente = sorted(lista, reverse=True)
-    print("Ordem crescente:", crescente)
-    print("Ordem decrescente:", decrescente)
+# C++ advanced
+# https://www.netacad.com/courses/c-plus-plus-advanced?courseLang=en-US&instance_id=b4fdd0ab-3676-4f20-81ac-4d3ffce2c8c3
